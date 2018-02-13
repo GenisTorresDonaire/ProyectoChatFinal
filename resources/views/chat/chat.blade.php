cef25a3b8accd9cd9131b5cc4e2baeff5b5e19d8
GenisTorresDonaire <gtorresdonaire@iesesteveterradas.cat>
2018-02-13 02:04:44 +0100

    casi acabado ya

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -1,38 +1,42 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="container d-flex justify-content-center ">
+    
     <div class="row" >
       <div class="col-md-10" style="background-color: white;">
         <h1>CHAT</h1>
         <hr>
         <div class="col-md-2">
             <div class="panel panel-default">
-                <div id="divAmigos" class="panel-heading"><b>Amigos</b></div>
+                <div id="divAmigos" class="panel-heading cabeceraChat" style="background: blue; color: white;"><b>Amigos</b></div>
                     
                 <div class="panel-body">
-                    <div>Manolo1</div>
-                    <div>Manolo2</div>
-                    <div>Manolo3</div>
-                    <div>Manolo4</div>
-                    <div>Manolo5</div>
-                    <div>Manolo6</div>
+                    <img id="imagenUser" src="images/pageLoader.gif"><b>Manolo1</b>
+                    <img id="imagenUser" src="images/pageLoader.gif"><b>Manolo2</b>
+                    <img id="imagenUser" src="images/pageLoader.gif"><b>Manolo3</b>
+                    <img id="imagenUser" src="images/pageLoader.gif"><b>Manolo4</b>
+                    <img id="imagenUser" src="images/pageLoader.gif"><b>Manolo5</b>
+                    <img id="imagenUser" src="images/pageLoader.gif"><b>Manolo6</b>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-10">
+        <div class="col-md-10 ">
             <div class="panel panel-default">
-                <div id="divChat" class="panel-heading"><b>Chat</b></div>
+                <div id="divChat" class="panel-heading cabeceraChat" style="background: blue; color: white;">
+                  <b>Chat</b>
+                </div>
                     
-                  <div class="panel-body">
-                      <div style="float: left;">
-                        <p><b>Manolo1: </b>Hola q tal?!!!</p>
-                      </div>
-                      <br>
-                      <div style="float: right;">
-                        <p><b>Tu: </b>Hola!! bien y tu?</p>
-                      </div>
-                  </div>
+                <div class="panel-body">
+                    <div style="float: left;">
+                      <p><b>Manolo1: </b>Hola q tal?!!!</p>
+                    </div>
+                    <br>
+                    <div style="float: right;">
+                      <p><b>Tu: </b>Hola!! bien y tu?</p>
+                    </div>
+                </div>
 
               </div>
           </div>
